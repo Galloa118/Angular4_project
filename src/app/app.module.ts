79152b11a0d72437d95fbbeb5275598b2e16bc5e
Galloa118 <pewallin0527@gmail.com>
2017-06-20 10:36:42 +0200

    282 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,30 @@ import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { RecipeService } from './recipes/recipe.service';
-
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+
   ],
   imports: [
     BrowserModule,
     HttpModule,
     AppRoutingModule,
-    RecipesModule,
     SharedModule,
     ShoppingListModule,
-    AuthModule
+    AuthModule,
+    CoreModule
 
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
